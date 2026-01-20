12029255035631d921e829285e8abdfce39dbbb9
banupriya2004 <120234043+banupriya2004@users.noreply.github.com>
2026-01-20 14:03:04 +0530

    Update MarketplaceApplication.java

--- marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
+++ marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
