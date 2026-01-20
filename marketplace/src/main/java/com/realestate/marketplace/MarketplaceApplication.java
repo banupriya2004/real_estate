b43ede885adaf29ae875d7f81caffc09c8bd0ad1
banupriya2004 <120234043+banupriya2004@users.noreply.github.com>
2026-01-20 13:05:32 +0530

    Update MarketplaceApplication.java

--- marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
+++ marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
@@ -9,5 +9,17 @@ public class MarketplaceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow ALL URLs
+                        .allowedOrigins("*") // Allow ALL websites (including Vercel)
+                        .allowedMethods("*") // Allow GET, POST, PUT, DELETE
+                        .allowedHeaders("*"); // Allow ALL headers
+            }
+        };
+    }
 
 }
