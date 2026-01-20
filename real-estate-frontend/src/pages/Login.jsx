456412c682a6fb67c55d6b13a235dcc3b6f2870f
banupriya2004 <120234043+banupriya2004@users.noreply.github.com>
2026-01-20 11:09:05 +0530

    Update Login.jsx

--- real-estate-frontend/src/pages/Login.jsx
+++ real-estate-frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
+      const response = await fetch("https://marketplace-backend-upn5.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
