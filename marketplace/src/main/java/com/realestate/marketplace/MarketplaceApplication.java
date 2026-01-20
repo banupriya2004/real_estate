ebe009bd131397e2879af0a8946443e744fce032
banupriya2004 <120234043+banupriya2004@users.noreply.github.com>
2026-01-20 13:49:21 +0530

    Update MarketplaceApplication.java

--- marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
+++ marketplace/src/main/java/com/realestate/marketplace/MarketplaceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*")
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
