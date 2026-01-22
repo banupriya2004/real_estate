d8b20964972663763d1e7ee7cdb3c04aaa23ed31
Banupriya M <banupriya.se22@bitsathy.ac.in>
2026-01-22 11:49:14 +0530

    updated backend

--- marketplace/src/main/java/com/realestate/marketplace/config/WebConfig.java
+++ marketplace/src/main/java/com/realestate/marketplace/config/WebConfig.java
@@ -19,7 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")               // apply to all API endpoints
-                .allowedOrigins("http://localhost:5173") // allow your React frontend
+                .allowedOrigins(
+                    "http://localhost:5173",           // Allow Local Frontend
+                    "https://real-estate-zmjv.vercel.app" // ✅ Allow Vercel Frontend
+                ) // allow your React frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
